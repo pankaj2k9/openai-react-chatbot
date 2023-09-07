c20252778bcb6aa7140f75efbdfc7f39f3bbe42f
pankaj2k9 <pkp2.me2k9@gmail.com>
2023-09-08 00:29:17 +0600

    intial setup done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from 'react';
+import 'styles/globals.css';
+import Layout from 'src/Container/Layout/Layout';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap.bundle');
+    }, []);
+
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
